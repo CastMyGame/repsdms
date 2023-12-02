e4e2858c2aeaacf462d5d32332bfc74aa2af024a
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2023-12-01 23:45:14 -0600

    added assigment controllers

---
+++ src/main/java/com/reps/demogcloud/models/assignments/Assignment.java
@@ -0,0 +1,64 @@
+package com.reps.demogcloud.models.assignments;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "assignments")
+public class Assignment {
+    @Id
+    private String assignmentName;
+    private int level;
+    private List<Question> questions;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Question {
+        private String question;
+        private String title;
+        private String body;
+        private List<Reference> references;
+        private List<RadioAnswer> radioAnswers;
+        private RetryQuestion retryQuestion;
+        private ExploratoryQuestions exploratoryQuestions;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Reference {
+        private String details;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class RadioAnswer {
+        private String value;
+        private String label;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class RetryQuestion {
+        private String imgUrl;
+        private String textToCompare;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ExploratoryQuestions {
+        private String exploratoryQuestion;
+        private String getExploratoryQuestionDescription;
+    }
+}
---
+++ src/main/java/com/reps/demogcloud/services/AssignmentService.java
@@ -0,0 +1,32 @@
+package com.reps.demogcloud.services;
+
+import com.reps.demogcloud.data.AssignmentRepository;
+import com.reps.demogcloud.models.assignments.Assignment;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class AssignmentService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final AssignmentRepository assignmentRepository;
+
+    public AssignmentService(AssignmentRepository assignmentRepository) {
+        this.assignmentRepository = assignmentRepository;
+    }
+
+
+    public List<Assignment> getAllAssignments(){
+       return  assignmentRepository.findAll();
+
+    }
+
+    public  Assignment createNewAssignment(Assignment assignments){
+        return assignmentRepository.save(assignments);
+    }
+}
