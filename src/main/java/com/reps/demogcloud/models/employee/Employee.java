c7ea5234dd5bc1dda551a792f8377fabf411d600
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2023-12-29 14:32:03 -0600

    added employee model

---
+++ src/main/java/com/reps/demogcloud/models/employee/Employee.java
@@ -0,0 +1,29 @@
+package com.reps.demogcloud.models.employee;
+
+import com.reps.demogcloud.security.models.RoleModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Document(collection = "employee")
+public class Employee {
+    @Id
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private RoleModel role;
+    private boolean isArchived;
+    private String archivedBy;
+    private LocalDateTime archivedOn;
+
+}
