e764e2f208be2abcf12b3de722a5ee2b0dbb1691
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2024-04-18 23:17:44 -0500

    added point added field

--- src/main/java/com/reps/demogcloud/models/punishment/PunishmentFormRequest.java
+++ src/main/java/com/reps/demogcloud/models/punishment/PunishmentFormRequest.java
@@ -18,5 +18,6 @@ public class PunishmentFormRequest {
     private String infractionPeriod;
     private String infractionDescription;
     private String teacherEmail;
+    private int points;
 
 }
