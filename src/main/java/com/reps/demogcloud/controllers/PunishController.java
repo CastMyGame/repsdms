dcad11febfc8a93ceab768defc725d29ce9e0eba
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2024-06-21 15:24:55 -0500

    create guidence, update notes, and get all guidance working

--- src/main/java/com/reps/demogcloud/controllers/PunishController.java
+++ src/main/java/com/reps/demogcloud/controllers/PunishController.java
@@ -2,6 +2,9 @@ package com.reps.demogcloud.controllers;
 
 
 import com.reps.demogcloud.models.ResourceNotFoundException;
+import com.reps.demogcloud.models.guidance.Guidance;
+import com.reps.demogcloud.models.guidance.GuidanceRequest;
+import com.reps.demogcloud.models.guidance.GuidanceResponse;
 import com.reps.demogcloud.models.punishment.*;
 import com.reps.demogcloud.services.PunishmentService;
 import lombok.RequiredArgsConstructor;
@@ -142,17 +145,16 @@ public class PunishController {
                 .body(message);
     }
 
-//    @PostMapping("/guidance/formList")
-//    public ResponseEntity<List<PunishmentResponse>> startGuidence(@RequestBody List<PunishmentFormRequest> punishmentListRequest) throws MessagingException, IOException, InterruptedException {
-//        var message = punishmentService.createGuidance(punishmentListRequest);
-//
-//        return ResponseEntity
-//                .accepted()
-//                .body(message);
-//    }
+    @PostMapping("/guidance/formList")
+    public ResponseEntity<List<GuidanceResponse>> startGuidence(@RequestBody List<GuidanceRequest> guidanceList) throws MessagingException, IOException, InterruptedException {
+        var message = punishmentService.createGuidance(guidanceList);
+        return ResponseEntity
+                .accepted()
+                .body(message);
+    }
 
     @PutMapping("/guidance/notes/{id}")
-    public ResponseEntity<Punishment> updateGuidance(@PathVariable String id,@RequestBody ThreadEvent event) throws MessagingException, IOException, InterruptedException {
+    public ResponseEntity<Guidance> updateGuidance(@PathVariable String id,@RequestBody ThreadEvent event) throws MessagingException, IOException, InterruptedException {
         var message = punishmentService.updateGuidance(id,event);
 
         return ResponseEntity
@@ -162,41 +164,41 @@ public class PunishController {
 
 
 
-    @PutMapping("/guidance/followup/{id}")
-    public ResponseEntity<Punishment> updateGuidanceFollowUp(@PathVariable String id, @RequestBody Map<String, String> payload) throws MessagingException, IOException, InterruptedException {
-        String scheduleFollowUp = payload.get("followUpDate");
-        String statusChange = payload.get("guidanceStatus");
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-
-        LocalDate followUpDate;
-        try {
-            followUpDate = LocalDate.parse(scheduleFollowUp, formatter);
-        } catch (DateTimeParseException e) {
-            return ResponseEntity.badRequest().body(null);  // or handle the error as appropriate
-        }
-        Punishment updatedPunishment = punishmentService.updateGuidanceFollowUp(id, followUpDate,statusChange);
-
-        return ResponseEntity.accepted().body(updatedPunishment);
-    }
-
-    @PutMapping("/guidance/status/{id}")
-    public ResponseEntity<Punishment> updateGuidanceStatus (@PathVariable String id, @RequestBody Map<String, String> payload) throws MessagingException, IOException, InterruptedException {
-        String newStatus = payload.get("status");
-        Punishment updatedPunishment = punishmentService.updateGuidanceStatus(id, newStatus);
-
-        return ResponseEntity.accepted().body(updatedPunishment);
-    }
-
-//    @GetMapping("/guidance/{status}/{userFilter}")
-//    public ResponseEntity<List<Punishment>> getAllGuidances(@PathVariable String status,@PathVariable  boolean userFilter) throws MessagingException, IOException, InterruptedException {
+//    @PutMapping("/guidance/followup/{id}")
+//    public ResponseEntity<Punishment> updateGuidanceFollowUp(@PathVariable String id, @RequestBody Map<String, String> payload) throws MessagingException, IOException, InterruptedException {
+//        String scheduleFollowUp = payload.get("followUpDate");
+//        String statusChange = payload.get("guidanceStatus");
 //
-//        List<Punishment> message = punishmentService.getAllGuidanceReferrals(status,userFilter);
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 //
-//        return ResponseEntity
-//                .accepted()
-//                .body(message);
+//        LocalDate followUpDate;
+//        try {
+//            followUpDate = LocalDate.parse(scheduleFollowUp, formatter);
+//        } catch (DateTimeParseException e) {
+//            return ResponseEntity.badRequest().body(null);  // or handle the error as appropriate
+//        }
+//        Punishment updatedPunishment = punishmentService.updateGuidanceFollowUp(id, followUpDate,statusChange);
+//
+//        return ResponseEntity.accepted().body(updatedPunishment);
 //    }
+
+//    @PutMapping("/guidance/status/{id}")
+//    public ResponseEntity<Punishment> updateGuidanceStatus (@PathVariable String id, @RequestBody Map<String, String> payload) throws MessagingException, IOException, InterruptedException {
+//        String newStatus = payload.get("status");
+//        Punishment updatedPunishment = punishmentService.updateGuidanceStatus(id, newStatus);
+//
+//        return ResponseEntity.accepted().body(updatedPunishment);
+//    }
+
+    @GetMapping("/guidance/{status}/{userFilter}")
+    public ResponseEntity<List<Guidance>> getAllGuidances(@PathVariable String status,@PathVariable  boolean userFilter) throws MessagingException, IOException, InterruptedException {
+
+        List<Guidance> message = punishmentService.getAllGuidanceReferrals(status,userFilter);
+
+        return ResponseEntity
+                .accepted()
+                .body(message);
+    }
 
 //    @PostMapping("/startPunish/adminReferral")
 //    public ResponseEntity<List<PunishmentResponse>> createNewAdminReferralBulk(@RequestBody List<PunishmentFormRequest> adminReferralListRequest) throws MessagingException, IOException, InterruptedException {
