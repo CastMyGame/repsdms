1879e4052cb6fcb22ac945d6485380c8ca6ce53d
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2024-06-26 00:26:38 -0500

    update controllers

---
+++ src/main/java/com/reps/demogcloud/models/punishment/ResourceOption.java
@@ -0,0 +1,12 @@
+package com.reps.demogcloud.models.punishment;
+
+import lombok.Data;
+
+@Data
+public class ResourceOption {
+    String value;
+    String label;
+    String url;
+    String category;
+
+}
---
+++ src/main/java/com/reps/demogcloud/models/punishment/ResourceUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.reps.demogcloud.models.punishment;
+
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class ResourceUpdateRequest {
+    private List<ResourceOption> resourceOptionList;
+
+    // Getters and Setters
+
+}
+
