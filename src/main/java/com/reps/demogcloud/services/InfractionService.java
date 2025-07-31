810e63099afbcef5409473394bfb9b261cd2e2ad
CastMyGame <castmygameinc@gmail.com>
2025-07-30 21:14:53 -0400

    Modularized InfractionService and separated code into specialized modules

--- src/main/java/com/reps/demogcloud/services/InfractionService.java
+++ src/main/java/com/reps/demogcloud/services/InfractionService.java
@@ -1,8 +1,10 @@
 package com.reps.demogcloud.services;
 
-import com.reps.demogcloud.data.InfractionRepository;
 import com.reps.demogcloud.exceptions.ResourceNotFoundException;
 import com.reps.demogcloud.models.infraction.Infraction;
+import com.reps.demogcloud.services.infraction.InfractionMutationService;
+import com.reps.demogcloud.services.infraction.InfractionQueryService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,45 +14,28 @@ import java.util.List;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class InfractionService {
+    private final InfractionMutationService infractionMutationService;
+    private final InfractionQueryService infractionQueryService;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private final InfractionRepository repository;
-
-    public InfractionService(InfractionRepository repository) {
-        this.repository = repository;
-    }
 
     public Infraction findInfractionByInfractionName (String infractionName) throws ResourceNotFoundException {
-        var findMe = repository.findByInfractionName(infractionName);
-
-        if (findMe == null) {
-            throw new ResourceNotFoundException("No infraction with that name exists");
-        }
-        logger.debug(String.valueOf(findMe));
-        return findMe;
+        return infractionQueryService.findInfractionByInfractionName(infractionName);
     }
 
     public Infraction findByInfractionId (String infractionId) throws ResourceNotFoundException {
-        var findMe = repository.findByInfractionId(infractionId);
-
-        if (findMe == null) {
-            throw new ResourceNotFoundException("No infraction with that ID exists");
-        }
-        logger.debug(String.valueOf(findMe));
-        return findMe;
-    }
-
-    public Infraction createNewInfraction (Infraction infraction ) {
-        return repository.save(infraction);
-    }
-    public String deleteInfraction ( Infraction infraction ) throws ResourceNotFoundException {
-        try{repository.delete(infraction);}
-        catch (Exception e) {
-            throw new ResourceNotFoundException("That infraction does not exist");
-        } return "${infraction} has been deleted";
+        return infractionQueryService.findByInfractionId(infractionId);
     }
 
     public List<Infraction> findAllInfractions() {
-        return repository.findAll();
+        return infractionQueryService.findAllInfractions();
+    }
+
+    public Infraction createNewInfraction (Infraction infraction ) {
+        return infractionMutationService.createNewInfraction(infraction);
+    }
+    public String deleteInfraction ( Infraction infraction ) throws ResourceNotFoundException {
+        return infractionMutationService.deleteInfraction(infraction);
     }
 }
